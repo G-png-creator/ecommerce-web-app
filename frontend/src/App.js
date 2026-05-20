b4d8a15af06b1c69b38f37418ee427bfe889eec8
ritisha12 <gritisha1612@gmail.com>
2026-05-20 16:24:58 +0530

    Final ecommerce project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
 
 function App() {
 
   return (
 
-    <BrowserRouter>
+    <div>
+      <Home />
+    </div>
 
-      <Navbar />
-
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/register" element={<Register />} />
-
-        <Route path="/cart" element={<Cart />} />
-
-        <Route path="/checkout" element={<Checkout />} />
-
-      </Routes>
-
-    </BrowserRouter>
   );
+
 }
 
 export default App;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,92 +1,201 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import "../App.css";
+import { useState } from "react";
 
 function Home() {
 
-    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
-    useEffect(() => {
+    const products = [
 
-        fetchProducts();
+        {
+            id:1,
+            name:"Wireless Headphones",
+            price:"₹2,999",
+            image:"https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+        },
 
-    }, []);
+        {
+            id:2,
+            name:"Smart Watch",
+            price:"₹4,499",
+            image:"https://images.unsplash.com/photo-1523275335684-37898b6baf30"
+        },
 
-    const fetchProducts = async () => {
+        {
+            id:3,
+            name:"Gaming Mouse",
+            price:"₹1,499",
+            image:"https://images.unsplash.com/photo-1527864550417-7fd91fc51a46"
+        },
 
-        try {
+        {
+            id:4,
+            name:"Laptop",
+            price:"₹59,999",
+            image:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
+        },
 
-            const response = await axios.get(
-                "http://localhost:5000/api/products"
-            );
+        {
+            id:5,
+            name:"iPhone 15 Pro",
+            price:"₹1,29,999",
+            image:"https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+        },
 
-            setProducts(response.data);
+        {
+            id:6,
+            name:"Bluetooth Speaker",
+            price:"₹3,299",
+            image:"https://images.unsplash.com/photo-1589003077984-894e133dabab"
+        },
+        {
+            id:7,
+            name:"DSLR Camera",
+            price:"₹74,999",
+            image:"https://images.unsplash.com/photo-1516035069371-29a1b244cc32"
+        },
 
-        } catch (error) {
-
-            console.log(error);
+        {
+            id:8,
+            name:"Nike Shoes",
+            price:"₹5,999",
+            image:"https://images.unsplash.com/photo-1542291026-7eec264c27ff"
         }
-    };
 
+    ];
+
+    // Add To Cart
     const addToCart = (product) => {
 
-        let cart =
-            JSON.parse(localStorage.getItem("cart")) || [];
+        setCart([...cart, product]);
 
-        cart.push(product);
-
-        localStorage.setItem(
-            "cart",
-            JSON.stringify(cart)
-        );
-
-        alert("Product Added To Cart");
     };
 
     return (
 
-        <div style={{ padding: "20px" }}>
+        <div className="home">
 
-            <h1>Products</h1>
+            {/* Navbar */}
+            <nav className="navbar">
 
-            <div
-                style={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    gap: "20px"
-                }}
-            >
+                <h1>Ritisha's Hub 🛍️</h1>
 
-                {
-                    products.map((product) => (
+                <div className="nav-links">
 
-                        <div
-                            key={product.id}
-                            style={{
-                                border: "1px solid black",
-                                padding: "20px",
-                                width: "250px"
-                            }}
-                        >
+                    <a href="/">Home</a>
+                    <a href="/">Products</a>
 
-                            <h2>{product.name}</h2>
+                    <div className="cart-icon">
+                        🛒 Cart ({cart.length})
+                    </div>
 
-                            <p>{product.description}</p>
+                </div>
 
-                            <h3>₹ {product.price}</h3>
+            </nav>
+
+            {/* Hero */}
+            <div className="hero">
+
+                <div className="hero-content">
+
+                    <h1>
+                        Style Meets Technology 💎 🛒
+                    </h1>
+
+                    <p>
+                                       Your one-stop destination for trendy fashion,electronics and premium accessories.
+                    </p>
+
+                    <button>
+                        Shop Now
+                    </button>
+
+                </div>
+
+            </div>
+
+            {/* Products */}
+            <div className="products-section">
+
+                <h2>
+                    Trending Products 🔥
+                </h2>
+
+                <div className="products-grid">
+
+                    {products.map((product) => (
+
+                        <div className="product-card" key={product.id}>
+
+                            <img
+                            src={product.image}
+                            alt={product.name}
+                            />
+
+                            <h3>
+                                {product.name}
+                            </h3>
+
+                            <p>
+                                {product.price}
+                            </p>
 
                             <button
-                                onClick={() => addToCart(product)}
+                            onClick={() => addToCart(product)}
                             >
                                 Add To Cart
                             </button>
 
                         </div>
+
+                    ))}
+
+                </div>
+
+            </div>
+
+            {/* Cart Section */}
+            <div className="cart-section">
+
+                <h2>
+                    Shopping Cart 🛒
+                </h2>
+
+                {cart.length === 0 ? (
+
+                    <p className="empty-cart">
+                        No products added yet.
+                    </p>
+
+                ) : (
+
+                    cart.map((item, index) => (
+
+                        <div className="cart-item" key={index}>
+
+                            <img
+                            src={item.image}
+                            alt={item.name}
+                            />
+
+                            <div>
+
+                                <h3>{item.name}</h3>
+
+                                <p>{item.price}</p>
+
+                            </div>
+
+                        </div>
+
                     ))
-                }
+
+                )}
 
             </div>
 
         </div>
+
     );
 }
 
